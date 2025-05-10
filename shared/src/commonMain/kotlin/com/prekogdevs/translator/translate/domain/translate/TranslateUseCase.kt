b109c892cf6e6fc1a1e2189da96cde0da9e8a21d
gergely-szebenyi <gergelyszebenyi@gmail.com>
2025-05-10 21:58:28 +0200

    Setting up translate use case

---
+++ shared/src/commonMain/kotlin/com/prekogdevs/translator/translate/domain/translate/TranslateUseCase.kt
@@ -0,0 +1,38 @@
+package com.prekogdevs.translator.translate.domain.translate
+
+import com.prekogdevs.translator.core.domain.language.Language
+import com.prekogdevs.translator.core.domain.util.Resource
+import com.prekogdevs.translator.translate.domain.history.HistoryDataSource
+import com.prekogdevs.translator.translate.domain.history.HistoryItem
+
+class TranslateUseCase(
+    private val client: TranslateClient,
+    private val historyDataSource: HistoryDataSource
+) {
+    suspend fun execute(
+        fromLanguage: Language,
+        fromText: String,
+        toLanguage: Language
+    ): Resource<String> {
+        return try {
+            val translatedText = client.translate(
+                fromLanguage, fromText, toLanguage
+            )
+
+            historyDataSource.insertHistoryItem(
+                HistoryItem(
+                    id = null,
+                    fromLanguageCode = fromLanguage.langCode,
+                    fromText = fromText,
+                    toLanguageCode = toLanguage.langCode,
+                    toText = translatedText,
+                )
+            )
+
+            Resource.Success(translatedText)
+        } catch(e: TranslateException) {
+            e.printStackTrace()
+            Resource.Error(e)
+        }
+    }
+}
