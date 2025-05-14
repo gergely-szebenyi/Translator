7cc44cb83ba6b17923a458c96a7d6fd7004c74f0
gergely-szebenyi <gergelyszebenyi@gmail.com>
2025-05-14 07:51:02 +0200

    Building the Translation Text Field

--- androidApp/src/main/java/com/prekogdevs/translator/android/core/presentation/TranslateScreen.kt
+++ androidApp/src/main/java/com/prekogdevs/translator/android/core/presentation/TranslateScreen.kt
@@ -1,19 +1,23 @@
 package com.prekogdevs.translator.android.core.presentation
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import com.prekogdevs.translator.R
+import android.widget.Toast
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalClipboardManager
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.unit.dp
 import com.prekogdevs.translator.android.core.presentation.components.LanguageDropDown
 import com.prekogdevs.translator.android.core.presentation.components.SwapLanguagesButton
+import com.prekogdevs.translator.android.core.presentation.components.TranslateTextField
 import com.prekogdevs.translator.translate.presentation.TranslateEvent
 import com.prekogdevs.translator.translate.presentation.TranslateState
 
@@ -22,6 +26,8 @@ fun TranslateScreen(
     state: TranslateState,
     onEvent: (TranslateEvent) -> Unit
 ) {
+    val context = LocalContext.current
+    val copiedText = stringResource(id = com.prekogdevs.translator.android.R.string.copied_to_clipboard)
     Scaffold(
         floatingActionButton = {
 
@@ -73,6 +79,46 @@ fun TranslateScreen(
                         }
                     )
                 }
+            }
+            item {
+                val clipboardManager = LocalClipboardManager.current
+                val keyboardController = LocalSoftwareKeyboardController.current
+                TranslateTextField(
+                    fromText = state.fromText,
+                    toText = state.toText,
+                    isTranslating = state.isTranslating,
+                    fromLanguage = state.fromLanguage,
+                    toLanguage = state.toLanguage,
+                    onTranslateClick = {
+                        keyboardController?.hide()
+                        onEvent(TranslateEvent.Translate)
+                    },
+                    onTextChange = {
+                        onEvent(TranslateEvent.ChangeTranslationText(it))
+                    },
+                    onCopyClick = { text ->
+                        clipboardManager.setText(
+                            buildAnnotatedString {
+                                append(text)
+                            }
+                        )
+                        Toast.makeText(
+                            context,
+                            copiedText,
+                            Toast.LENGTH_LONG
+                        ).show()
+                    },
+                    onCloseClick = {
+                        onEvent(TranslateEvent.CloseTranslation)
+                    },
+                    onSpeakerClick = {
+
+                    },
+                    onTextFieldClick = {
+                        onEvent(TranslateEvent.EditTranslation)
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                )
             }
         }
     }
