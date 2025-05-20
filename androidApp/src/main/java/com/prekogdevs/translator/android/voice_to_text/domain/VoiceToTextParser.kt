0e3ee975eace0ed4fb5eaa5fd97b917b80c47d4c
gergely-szebenyi <gergelyszebenyi@gmail.com>
2025-05-20 17:25:27 +0200

    Implementing Android speech recognition

---
+++ androidApp/src/main/java/com/prekogdevs/translator/android/voice_to_text/domain/VoiceToTextParser.kt
@@ -0,0 +1,11 @@
+package com.prekogdevs.translator.android.voice_to_text.domain
+
+import com.prekogdevs.translator.core.domain.util.CommonStateFlow
+
+interface VoiceToTextParser {
+    val state: CommonStateFlow<VoiceToTextParserState>
+    fun startListening(languageCode: String)
+    fun stopListening()
+    fun cancel()
+    fun reset()
+}
